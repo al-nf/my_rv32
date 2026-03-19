b2bd6dada95166b4a99aece9e7256fce8d06e66b
al-nf <alankjfung@gmail.com>
2026-03-19 11:34:58 -0700

    modularization!

---
+++ lib/mux2.ml
@@ -0,0 +1,24 @@
+open Hardcaml
+open Signal
+
+module Make (Config: sig val bit_width : int end) = struct
+  open Config
+
+  module I = struct
+    type 'a t = {
+      in0: 'a; [@bits bit_width]
+      in1: 'a; [@bits bit_width]
+      sel: 'a; [@bits 1]
+    } [@@deriving hardcaml]
+  end
+
+  module O = struct
+    type 'a t = {
+      mux_output: 'a; [@bits bit_width]
+    } [@@deriving hardcaml]
+  end
+
+  let create _scope (i: _ I.t) = {
+    O.mux_output = mux i.sel [i.in0; i.in1]
+  }
+end
