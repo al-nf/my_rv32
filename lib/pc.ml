66864083cbb29e372b0c5821b90632d2024ab54b
al-nf <alankjfung@gmail.com>
2026-03-19 18:03:03 -0700

    pc done !

---
+++ lib/pc.ml
@@ -0,0 +1,27 @@
+open Hardcaml
+open Signal
+
+module Make = struct
+  module I = struct
+    type 'a t = {
+       clk: 'a [@bits 1];
+       rst: 'a [@bits 1];
+       pc_in: 'a [@bits 32];
+       pc_wr: 'a [@bits 1];
+    } [@@deriving hardcaml]
+  end
+
+  module O = struct
+    type 'a t = {
+      pc_out: 'a [@bits 32];
+    } [@@deriving hardcaml]
+  end
+
+  let create _scope (i: _ I.t) =
+    let spec = Reg_spec.create ~clock:i.clk ~reset:i.rst () in
+    let pc = reg_fb spec ~enable:vdd ~width:32 ~f:(fun pc ->
+      mux2 i.rst (zero 32) (mux2 i.pc_wr i.pc_in (pc +: of_int ~width:32 4))
+    ) in
+    { O.pc_out = pc }
+
+end
